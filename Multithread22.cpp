c8cf825ba1456bdebdefe123e3db0d45c843fc24
dmartinez1100 <38766193+dmartinez1100@users.noreply.github.com>
2018-11-06 15:51:46 -0500

    Add files via upload

--- Multithread22.cpp
+++ Multithread22.cpp
@@ -4,17 +4,31 @@
 #include<future>
 using namespace std;
 int fibo(int n);
+int fibonop(int n);
 
 int main(){
-    for(int value=0 ;value<=12;value++)
-	cout<<fibo(value)<<" ";
+	cout<<fibo(24)<<" "<<flush;
+	//cout<<fibonop(24)<<" "<<flush;
 	cout<<endl;
     return 0;
 }
 
 int fibo(int n){
+	//cout<<n<<" ";
+	future<int> fn;
+	int x;
 	if(n<=1)
 		return n;
 	else
-		return fibo(n-1)+fibo(n-2);
+		fn= async(launch::async,fibo,n-1);
+		x=fn.get();
+		return x+fibo(n-2);
+}
+
+int fibonop(int n){
+	//cout<<n<<" ";
+	if(n<=1)
+		return n;
+	else
+		return fibonop(n-1)+fibonop(n-2);
 }
